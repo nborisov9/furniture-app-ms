8322d44766f58e6d87711c33b5119a772378d6d0
Nikita <nik-borisov-1999@maul.ru>
2020-11-27 11:10:58 +0400

    cart layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,59 +9,61 @@ export const Header = () => {
   const { totalPrice, totalCount } = useSelector(({ cart }) => cart)
 
   return (
-    <div className="header">
-      <div className="header__top top-header">
-        <div className="top-header__wrapper">
-          <div className="top-header__list">
-            <li className="top-header__link">Главная</li>
-            <li className="top-header__link">О магазине</li>
-            <li className="top-header__link">О нас</li>
-            <li className="top-header__link">Контакты</li>
-            <li className="top-header__link">Отзывы</li>
+    <header>
+      <div className="header">
+        <div className="header__top top-header">
+          <div className="top-header__wrapper">
+            <div className="top-header__list">
+              <li className="top-header__link">Главная</li>
+              <li className="top-header__link">О магазине</li>
+              <li className="top-header__link">О нас</li>
+              <li className="top-header__link">Контакты</li>
+              <li className="top-header__link">Отзывы</li>
+            </div>
+            <div className="top-header__social">
+              <a href="/" className="social-mail">
+                <i className="far fa-envelope"></i>
+                nik-borisov-1999@mail.ru
+              </a>
+              <a href="/" className="social-phone">
+                <i className="fas fa-phone"></i>
+              </a>
+              <a href="/" className="social-telegram">
+                <i className="fab fa-telegram-plane"></i>
+              </a>
+              <a href="/" className="social-instagram">
+                <i className="fab fa-instagram"></i>
+              </a>
+            </div>
           </div>
-          <div className="top-header__social">
-            <a href="/" className="social-mail">
-              <i className="far fa-envelope"></i>
-              nik-borisov-1999@mail.ru
-            </a>
-            <a href="/" className="social-phone">
-              <i className="fas fa-phone"></i>
-            </a>
-            <a href="/" className="social-telegram">
-              <i className="fab fa-telegram-plane"></i>
-            </a>
-            <a href="/" className="social-instagram">
-              <i className="fab fa-instagram"></i>
-            </a>
+        </div>
+        <div className="header__botom bottom-header">
+          <div className="bottom-header__wrapper">
+            <div className="header-logo">
+              <Link to="/">
+                <div className="bottom-header__logo">Furniture Store</div>
+              </Link>
+              <div className="header-vertical-line"></div>
+              <div className="header-qoute">лучшая мебель во вселенной</div>
+            </div>
+            <div className="bottom-header__cart">
+              <Link to="/cart">
+                <Button className="button-cart button">
+                  <div className="button-price-icon">
+                    <span>{totalPrice}</span>
+                    <i className="fas fa-ruble-sign"></i>
+                  </div>
+                  <img src={line} alt="" />
+                  <div className="button-cart-icon">
+                    <i className="fas fa-shopping-basket"></i>
+                    <span>{totalCount}</span>
+                  </div>
+                </Button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-      <div className="header__botom bottom-header">
-        <div className="bottom-header__wrapper">
-          <div className="header-logo">
-            <Link to="/">
-              <div className="bottom-header__logo">Furniture Store</div>
-            </Link>
-            <div className="header-vertical-line"></div>
-            <div className="header-qoute">лучшая мебель во вселенной</div>
-          </div>
-          <div className="bottom-header__cart">
-            <Link to="/cart">
-              <Button className="button-cart button">
-                <div className="button-price-icon">
-                  <span>{totalPrice}</span>
-                  <i className="fas fa-ruble-sign"></i>
-                </div>
-                <img src={line} alt="" />
-                <div className="button-cart-icon">
-                  <i className="fas fa-shopping-basket"></i>
-                  <span>{totalCount}</span>
-                </div>
-              </Button>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
+    </header>
   )
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,111 @@
 import React from 'react'
 
+import { Button } from '../components'
+
 export const Cart = () => {
   return (
-    <div className="container">
-      <div style={{ fontWeight: 600, fontSize: '50px' }}>Cart</div>
+    <div className="wrapper-cart">
+      <div className="cart">
+        <div className="cart__top">
+          <div className="cart__title">
+            <i className="fas fa-cart-arrow-down"></i>
+            Корзина
+          </div>
+          <div className="cart__clear btn-cart-hover">
+            <i className="far fa-trash-alt"></i>
+            очистить корзину
+          </div>
+        </div>
+        <div className="cart__item item-cart">
+          <div className="item-cart__info">
+            <img
+              src="https://www.ikea.com/ru/ru/images/products/norrake…oker-skamya-bereza__0682550_PE720526_S5.JPG?f=xxs"
+              alt=""
+            />
+            <div className="item-cart__body">
+              <div className="item-cart__name">LEIFARNE</div>
+              <div className="item-cart__details">
+                <div className="item-cart__type">
+                  тип: <span>цельный</span>
+                </div>
+                <div className="item-cart__size">
+                  размер: <span>100х100 см</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="item-cart__arrow">
+            <i className="fas fa-minus btn-cart-hover"></i>
+            <span>3</span>
+            <i className="fas fa-plus btn-cart-hover"></i>
+          </div>
+          <div className="item-cart__price">
+            <span>
+              2000
+              <i className="fas fa-ruble-sign"></i>
+            </span>
+          </div>
+          <div className="item-cart__close btn-cart-hover">
+            <i className="far fa-times-circle"></i>
+          </div>
+        </div>
+        <div className="cart__item item-cart">
+          <div className="item-cart__info">
+            <img
+              src="https://www.ikea.com/ru/ru/images/products/norrake…oker-skamya-bereza__0682550_PE720526_S5.JPG?f=xxs"
+              alt=""
+            />
+            <div className="item-cart__body">
+              <div className="item-cart__name">LEIFARNE</div>
+              <div className="item-cart__details">
+                <div className="item-cart__type">
+                  тип: <span>цельный</span>
+                </div>
+                <div className="item-cart__size">
+                  размер: <span>100х100 см</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="item-cart__arrow">
+            <i className="fas fa-minus btn-cart-hover"></i>
+            <span>3</span>
+            <i className="fas fa-plus btn-cart-hover"></i>
+          </div>
+          <div className="item-cart__price">
+            <span>
+              2000
+              <i className="fas fa-ruble-sign"></i>
+            </span>
+          </div>
+          <div className="item-cart__close btn-cart-hover">
+            <i className="far fa-times-circle"></i>
+          </div>
+        </div>
+        <div className="cart__result result-cart">
+          <div className="result-cart__length">
+            Всего стульев: <span>3</span> <i>шт.</i>
+          </div>
+          <div className="result-cart__total">
+            Сумма заказа:
+            <span>
+              3000
+              <i className="fas fa-ruble-sign"></i>
+            </span>
+          </div>
+        </div>
+        <div className="cart__bottom">
+          <Button className="btn-exit btn-cart-hover">
+            <i className="fas fa-angle-left"></i>
+            Вeрнуться назад
+          </Button>
+          <Button className="button-cart button btn-pay">
+            <span>Оплатить сейчас</span>
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
